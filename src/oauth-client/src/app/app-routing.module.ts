ff583f3299601275e21dbc6a48cbdcec42e5f906
Pobx <geidtiphong.s@allrightbox.com>
2020-10-14 14:17:37 +0700

    edit AppRoutingModule

--- src/oauth-client/src/app/app-routing.module.ts
+++ src/oauth-client/src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './account/register/register.component';
 
-
+const routes: Routes = [{ path: 'register', component: RegisterComponent }];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
