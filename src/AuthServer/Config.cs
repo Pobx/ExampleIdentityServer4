b117c2a9e4ce7116e1e2b8a606d87f598deadb81
Pobx <geidtiphong.s@allrightbox.com>
2020-10-17 15:45:48 +0700

    add ApiResources

--- src/AuthServer/Config.cs
+++ src/AuthServer/Config.cs
@@ -42,9 +42,19 @@ namespace AuthServer {
   // }
 
   public static class Config {
+
     public static IEnumerable<ApiScope> ApiScopes => new List<ApiScope> {
       new ApiScope ("api1", "My Api")
     };
+
+    public static IEnumerable<ApiResource> ApiResources =>
+      new List<ApiResource> {
+        new ApiResource ("resourceapi", "Resource API") {
+        Scopes = new List<string> () {
+        "api1"
+        }
+        }
+      };
 
     public static IEnumerable<Client> Clients => new List<Client> {
       new Client {
@@ -58,6 +68,19 @@ namespace AuthServer {
       // Scopes ที่ client สามารถเข้าถึงได้
       AllowedScopes = { "api1" }
 
+      },
+
+      new Client {
+      RequireConsent = false,
+      ClientId = "angular_spa",
+      ClientName = "Angular SPA",
+      AllowedGrantTypes = GrantTypes.Implicit,
+      AllowedScopes = { "openid", "profile", "email", "api1" },
+      RedirectUris = { "http://localhost:4200/auth-callback" },
+      PostLogoutRedirectUris = { "http://localhost:4200/" },
+      AllowedCorsOrigins = { "http://localhost:4200" },
+      AllowAccessTokensViaBrowser = true,
+      AccessTokenLifetime = 3600
       }
     };
 
