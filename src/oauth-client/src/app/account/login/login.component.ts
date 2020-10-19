6f25739d4308839dba31fd848cbdb98a1a5e15a2
Pobx <geidtiphong.s@allrightbox.com>
2020-10-19 09:56:29 +0700

    inject AuthService, add RequestLoginModel to LoginComponent

--- src/oauth-client/src/app/account/login/login.component.ts
+++ src/oauth-client/src/app/account/login/login.component.ts
@@ -1,17 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { AuthService } from 'src/app/core/authentication/auth.service';
+import { RequestLoginModel } from 'src/app/shared/models/RequestLoginModel';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  requestRegisterModel: RequestLoginModel;
+  myForm: FormGroup;
+  constructor(private fb: FormBuilder, private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.requestRegisterModel = {
+      email: null,
+      password: null,
+    };
+
+    this.myForm = this.fb.group({
+      email: [null],
+      password: [null],
+    });
   }
 
-  onSubmit() {}
-
+  onSubmit() {
+    
+  }
 }
