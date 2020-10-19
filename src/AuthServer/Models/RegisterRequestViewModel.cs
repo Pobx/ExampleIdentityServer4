7dda17515d07d59930352d833c452ddbf60d27a2
Pobx <geidtiphong.s@allrightbox.com>
2020-10-19 10:19:15 +0700

    create LoginRequestViewModel class for receive data from SPA

--- src/AuthServer/Models/RegisterRequestViewModel.cs
+++ src/AuthServer/Models/RegisterRequestViewModel.cs
@@ -13,7 +13,21 @@ namespace AuthServer.Models {
     public string Email { get; set; }
 
     [Required]
-    [StringLength (100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+    [StringLength (100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 4)]
+    [DataType (DataType.Password)]
+    [Display (Name = "Password")]
+    public string Password { get; set; }
+  }
+
+  public class LoginRequestViewModel {
+
+    [Required]
+    [EmailAddress]
+    [Display (Name = "Email")]
+    public string Email { get; set; }
+
+    [Required]
+    [StringLength (100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 4)]
     [DataType (DataType.Password)]
     [Display (Name = "Password")]
     public string Password { get; set; }
