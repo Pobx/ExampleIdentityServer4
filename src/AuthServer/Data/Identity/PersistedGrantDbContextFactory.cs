6b4cfa650ce9fc4f736733c72931efba970181eb
Pobx <geidtiphong.s@allrightbox.com>
2020-11-03 09:45:14 +0700

    comment all line of code PersistedGrantDbContextFactor.cs

--- src/AuthServer/Data/Identity/PersistedGrantDbContextFactory.cs
+++ src/AuthServer/Data/Identity/PersistedGrantDbContextFactory.cs
@@ -6,16 +6,16 @@ using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 
-namespace AuthServer.Data.Identity {
-  public class PersistedGrantDbContextFactory : IDesignTimeDbContextFactory<PersistedGrantDbContext> {
+// namespace AuthServer.Data.Identity {
+//   public class PersistedGrantDbContextFactory : IDesignTimeDbContextFactory<PersistedGrantDbContext> {
 
-    private const string connectionString ="Server=localhost,1433;Database=AuthServer;User Id=sa;Password=P@ssword1234;";
-    public PersistedGrantDbContext CreateDbContext (string[] args) {
-      var configuration = new ConfigurationBuilder();
-      var optionsBuilder = new DbContextOptionsBuilder<PersistedGrantDbContext> ();
-      optionsBuilder.UseSqlServer (connectionString,
-        sql => sql.MigrationsAssembly (typeof (PersistedGrantDbContextFactory).GetTypeInfo ().Assembly.GetName ().Name));
-      return new PersistedGrantDbContext (optionsBuilder.Options, new OperationalStoreOptions ());
-    }
-  }
-}
+//     private const string connectionString ="Server=localhost,1433;Database=AuthServer;User Id=sa;Password=P@ssword1234;";
+//     public PersistedGrantDbContext CreateDbContext (string[] args) {
+//       var configuration = new ConfigurationBuilder();
+//       var optionsBuilder = new DbContextOptionsBuilder<PersistedGrantDbContext> ();
+//       optionsBuilder.UseSqlServer (connectionString,
+//         sql => sql.MigrationsAssembly (typeof (PersistedGrantDbContextFactory).GetTypeInfo ().Assembly.GetName ().Name));
+//       return new PersistedGrantDbContext (optionsBuilder.Options, new OperationalStoreOptions ());
+//     }
+//   }
+// }
