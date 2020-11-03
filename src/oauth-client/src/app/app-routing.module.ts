ad7defd50259327744eb20e990a940938f0ea06e
Pobx <geidtiphong.s@allrightbox.com>
2020-11-03 10:32:40 +0700

    add HomeComponent to app-routing.module

--- src/oauth-client/src/app/app-routing.module.ts
+++ src/oauth-client/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthCallbackComponent } from './account/auth-callback/auth-callback.component';
+import { HomeComponent } from './account/home/home.component';
 import { LoginComponent } from './account/login/login.component';
 import { RegisterComponent } from './account/register/register.component';
 
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'auth-callback', component: AuthCallbackComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'home', component: HomeComponent },
   { path: '**', redirectTo: 'register' },
 ];
 
