1eea4b4e6342334113a7d880a9b1bea25061aa7f
Pobx <geidtiphong.s@allrightbox.com>
2020-11-03 10:28:26 +0700

    add AuthCallbackComponent to app-routing.module.cs

--- src/oauth-client/src/app/app-routing.module.ts
+++ src/oauth-client/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthCallbackComponent } from './account/auth-callback/auth-callback.component';
 import { LoginComponent } from './account/login/login.component';
 import { RegisterComponent } from './account/register/register.component';
 
 const routes: Routes = [
   { path: 'register', component: RegisterComponent },
-  { path: 'auth-callback', component: RegisterComponent },
+  { path: 'auth-callback', component: AuthCallbackComponent },
   { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: 'register' },
 ];
